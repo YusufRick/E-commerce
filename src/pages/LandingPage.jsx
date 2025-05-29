5b7a17564be253e9b686f64e1bca56292706d857
amirziqry <amirziqry8@gmail.com>
2025-05-29 14:54:08 +0800

    logo

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,14 +4,15 @@ import { motion } from 'framer-motion';
 export default function LandingPage({ go }) {
   return (
     <div className="landing">
-      <motion.h1
-        className="site-title"
-        initial={{ y: 50, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ delay: 0.5, duration: 0.8 }}
-      >
-        KASAHARA
-      </motion.h1>
+      <motion.img
+  src={logo}
+  alt="Site Logo"
+  className="site-logo"
+  initial={{ y: 50, opacity: 0 }}
+  animate={{ y: 0, opacity: 1 }}
+  transition={{ delay: 0.5, duration: 0.8 }}
+/> 
+
 
       <button
         className="start-btn"
