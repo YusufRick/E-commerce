7ebeb440c71e4e735f244e3bd78ed1eee48797b3
YusufRick <c3022740@hallam.shu.ac.uk>
2025-06-23 15:14:20 +0100

    changes

--- src/pages/Visual.jsx
+++ src/pages/Visual.jsx
@@ -8,6 +8,7 @@ import Cart from '../components/Cart';
 import visual1 from '../assets/visual1.png';
 import visual2 from '../assets/visual2.gif';
 import visual3 from '../assets/visual3.png';
+import visual4 from '../assets/visual4.gif';
 
 import '../Visual.css';
 
@@ -15,6 +16,7 @@ const items = [
   { id: 1, name: 'Visual 1', description: '', image: visual1 },
   { id: 2, name: 'Visual 2', description: '', image: visual2 },
   { id: 3, name: 'Visual 3', description: '', image: visual3 },
+  { id: 3, name: 'Visual 4', description: '', image: visual4 },
 
 ];
 
--- src/pages/viewVisual.jsx
+++ src/pages/viewVisual.jsx
@@ -4,11 +4,13 @@ import { CartContext } from '../context/CartContext';
 import visual1 from '../assets/visual1.png';
 import visual2 from '../assets/visual2.gif';
 import visual3 from '../assets/visual3.png';
+import visual4 from '../assets/visual4.gif'
 
-const visuals = [
+const items = [
   { id: 1, name: 'Visual 1', description: '', image: visual1 },
   { id: 2, name: 'Visual 2', description: '', image: visual2 },
   { id: 3, name: 'Visual 3', description: '', image: visual3 },
+  { id: 4, name: 'Visual 4', description:'', image: visual4}
 
 ];
 
@@ -16,7 +18,7 @@ export default function ViewVisual() {
   const { id } = useParams();
   const navigate = useNavigate();
   const { addToCart } = useContext(CartContext);
-  const visual = visuals[id];
+  const visual = items[id-1];
 
   return (
     <div className="product-page">
